195689b26a0091f206e458ce6612cb87b09c5135
lingraj <lingrajmalipatil1@gmail.com>
2026-05-25 01:34:06 +0530

    Configure frontend client as SPA with Vercel rewrites

--- src/client/react-router.config.js
+++ src/client/react-router.config.js
@@ -2,6 +2,6 @@ import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   // Server-side render by default. Set to false to enable SPA mode.
-  ssr: true,
+  ssr: false,
   presets: process.env.VERCEL ? [vercelPreset()] : [],
 };
